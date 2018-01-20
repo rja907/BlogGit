d076c3d95c46c77d623a83f6ed7922079a10d6a2
rja907 <kmrrajj@gmail.com>
2018-01-21 03:51:27 +0530

    posts fix, new post setup

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -6,18 +7,27 @@ class PostsIndex extends Component {
   componentDidMount(){
     this.props.fetchPosts();
   }
+  renderPosts(){
+    return _.map(this.props.posts, post=> {
+      return(
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
   render() {
-    console.log(this.props);
     return(
       <div>
-        Posts Index!
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
       </div>
     )
   }
 }
 
 function mapStateToProps(state){
-  return { posts: state.props};
+  return { posts: state.posts};
 }
 
 export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
